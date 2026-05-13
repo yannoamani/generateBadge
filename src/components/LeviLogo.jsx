885f4229d391d2dcca53ce8987d72555ba6f3fd9
amani beda yann clarance ANON <amaniyann5500@gmail.com>
2026-05-13 14:09:03 +0000

    Mise à jour application badge

--- src/components/LeviLogo.jsx
+++ src/components/LeviLogo.jsx
@@ -1,7 +1,7 @@
 export default function LeviLogo({ size = 64, className = '' }) {
   return (
     <img
-      src="/logo.jpeg"
+      src="/images/logo.jpeg"
       alt="Logo Tribu Lévi"
       width={size}
       height={size}
